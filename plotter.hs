7e62862e4ded9bbe60668e005ad781f3e47998c3
Kajetan Bojko <kai@shutl.co.uk>
2014-08-25 15:56:40 +0100

    trying animation of spools

--- plotter.hs
+++ plotter.hs
@@ -1,70 +1,103 @@
 import Graphics.Gloss
 
---type Line = (Point, Point)
---data Step = L | R | LR
+type Size = (Float, Float)
+data Rot = L | R | N deriving (Eq, Show)
+type Step = (Rot, Rot)
+type History = ([Rot], Maybe Rot)
 
---leftSpool = (-100, 300)
---rightSpool = (400, 300)
---step = 5
+leftSpoolPoint = (-250, 200)
+rightSpoolPoint = (250, 200)
+canvasSize = (300, 200)
 
---steps :: Line -> [Step]
+timePerStep = 1 :: Float
+degreesPerStep = 45 :: Float
+
+spoolRadius = 10 :: Float
+
+steps = [ (L, R)
+        , (N, L)
+        , (R, R) ]
 
 main :: IO ()
 main 
  =  animate (InWindow "Plotter" (800, 600) (5, 5))
-                (greyN 0.2)
+                black
     frame 
 
-
--- Produce one frame of the animation.
 frame :: Float -> Picture
 frame timeS
- = Pictures 
-  -- the red rectangles
-  [ Translate 0 150   backRec
-  , Translate 0 0   backRec
-  , Translate 0 (-150)  backRec
+ = Pictures [ spools timeS
+            , canvasPic ]
 
-  -- the tree
-  , Translate 0 (-150) $  treeFrac 7 timeS
-  ]
+spools :: Float -> Picture
+spools timeS = Pictures
+  [ trans leftSpoolPoint (Rotate degreesLeft spoolPic)
+  , trans rightSpoolPoint (Rotate degreesRight spoolPic) ]
+  where
+    degreesLeft = spoolDegrees (leftRots steps) (splitTime timeS)
+    degreesRight = spoolDegrees (rightRots steps) (splitTime timeS)
 
-
--- One of the red backing rectangles, with a white outline.
-backRec :: Picture
-backRec 
- = Pictures
-  [ Color red   (rectangleSolid 400 100)
-  , Color white (rectangleWire  400 100) ]
-
-
--- The color for the outline of the tree's branches.
-treeOutline :: Color
-treeOutline = makeColor 0.3 0.3 1.0 1.0
+spoolDegrees :: [Rot] -> (Int, Float) -> Float
+spoolDegrees rots (completeSteps, current) = steps * degreesPerStep
+  where
+    steps = histToSteps hist current
+    hist = history rots completeSteps
 
 
--- The color for the shading of the tree's branches.
---  The Alpha here is set to 0.5 so the branches are partly transparent.
-treeColor :: Color
-treeColor = makeColor 0.0 1.0 0.0 0.5
+trans :: Point -> Picture -> Picture
+trans (x, y) pic = Translate x y pic
+
+spoolPic :: Picture
+spoolPic = Pictures [ Color white (circle spoolRadius)
+                    , Color white (line [(0, 0), (spoolRadius, 0)]) ]
+
+canvasPic :: Picture
+canvasPic = color (greyN 0.2) (rectangleWire width height)
+  where
+    width = fst canvasSize
+    height = snd canvasSize
 
 
--- The tree fractal.
---  The position of the branches changes depending on the animation time
---  as well as the iteration number of the fractal.
-treeFrac :: Int -> Float -> Picture
-treeFrac 0 timeS = Blank
-treeFrac n timeS
- = Pictures
-  [ Color treeColor   $ rectangleUpperSolid 20 300
-  , Color treeOutline $ rectangleUpperWire  20 300
-  , Translate 0 30
-    $ Rotate  (200 * sin timeS / (fromIntegral n) )
-    $ Scale   0.9 0.9 
-    $ treeFrac (n-1) timeS
+count :: Eq a => a -> [a] -> Int
+count elem list = length $ filter (\x -> x == elem) list
 
-  , Translate 0 70
-    $ Rotate  (-200 * sin timeS / (fromIntegral n))
-    $ Scale   0.8 0.8 
-    $ treeFrac (n-1) timeS
-  ]
+neutralize :: [Rot] -> Int
+neutralize rots = (count R rots) - (count L rots)
+
+history :: [Rot] -> Int -> History
+history rots completeStepCount = 
+  (take completeStepCount rots, elementAt rots completeStepCount)
+
+elementAt :: [a] -> Int -> Maybe a
+elementAt list index = if length list <= index
+  then Nothing
+  else Just $ list !! index
+
+splitTime :: Float -> (Int, Float)
+splitTime time = (completeStepCount, remainder)
+  where
+    steps = time / timePerStep
+    completeStepCount = floor steps
+    remainder = (time - (fromIntegral completeStepCount) * timePerStep) / timePerStep
+
+histToSteps :: History -> Float -> Float
+histToSteps (complete, inProgress) remainder =
+  (fromIntegral $ neutralize complete) + fractional
+  where
+    fractional = case inProgress of
+      Nothing -> 0
+      Just rot -> remainder * (direction rot)
+
+direction :: Rot -> Float
+direction L = -1
+direction R = 1
+direction N = 0
+
+leftRots :: [Step] -> [Rot]
+leftRots = map fst
+
+rightRots :: [Step] -> [Rot]
+rightRots = map snd
+
+
+ --makeColor 0.3 0.3 1.0 1.0
