b8810d2508a483e8557826d58af0584737f19ba7
Kajetan Bojko <kai@shutl.co.uk>
2014-09-10 07:45:58 +0100

    parser ignores whitespace

--- Plotter/HpglParser.hs
+++ Plotter/HpglParser.hs
@@ -1,6 +1,7 @@
 module Plotter.HpglParser (parseHPGL) where
 
 import Text.ParserCombinators.Parsec
+import Data.Char
 
 data HPGLCommand = PD | PU | MV (Float, Float) |
   SC (Float, Float, Float, Float) deriving Show
@@ -8,8 +9,8 @@ data HPGLCommand = PD | PU | MV (Float, Float) |
 hpglFile = sepBy cmd (oneOf ";\n")
 cmd = try (string "PU" >> return [Just PU])
   <|> try (string "PD" >> return [Just PD])
-  <|> try (string "PA" >> moves)
-  <|> try (string "SC" >> scale)
+  <|> (string "PA" >> moves)
+  <|> (string "SC" >> scale)
   <|> ((many (noneOf ";")) >> return [Nothing])
 
 moves = sepBy pair (char ',')
@@ -28,10 +29,11 @@ scale = do
                     , a !! 3 )]
 
 parseHPGL :: String -> Either ParseError [HPGLCommand]
-parseHPGL input = fmap (filter . flatten) (parse hpglFile "(unknown)" input)
+parseHPGL input = fmap (ignore . flatten) $ parse hpglFile "(unknown)" $ cleanInput input
   where
+    cleanInput = filter (not . isSpace)
     flatten = foldl1 (++)
-    filter = foldr rejectNothings []
+    ignore = foldr rejectNothings []
       where
         rejectNothings e a = case e of 
           Nothing -> a
