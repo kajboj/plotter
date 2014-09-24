04141b64500dc43f946b01ad843be029691409ca
Kajetan Bojko <kai@shutl.co.uk>
2014-09-24 20:32:18 +0100

    removed unused code

--- Driver.hs
+++ Driver.hs
@@ -3,21 +3,8 @@ import Plotter.CommandWriter
 import Plotter.Shared
 import Plotter.HpglParser
 
-
-hpgl = foldl1 (++) $ replicate 10 [ PU
-                                  , SC (-300, 300, -300, 300)
-                                  , MV (-100, -100)
-                                  , PD
-                                  , MV (-100, 100)
-                                  , MV (100, 100)
-                                  , MV (100, -100)
-                                  , MV (-100, -100) 
-                                  ]
-
-
 main :: IO ()
-main 
- = do 
+main = do
   s <- getContents
   case parseHPGL s of
     Left error -> putStrLn (show error)
