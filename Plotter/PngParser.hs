200e1f1e386fe5a75bfc84eb643c765d77d3347a
Kajetan Bojko <kai@shutl.co.uk>
2014-10-26 21:03:03 +0000

    parsing grayscale values out of png file

---
+++ Plotter/PngParser.hs
@@ -0,0 +1,18 @@
+import Codec.Picture
+
+parsePng :: String -> IO [[Int]]
+parsePng filepath = do
+  ei <- readPng filepath
+  case ei of
+    Left err -> error err
+    Right dynamicImage -> return $ process dynamicImage
+
+
+process :: DynamicImage -> [[Int]]
+process (ImageRGB8 image) = map (map gray) coords
+  where
+    gray (x, y) = case pixelAt image x y of
+      PixelRGB8 r _ _ -> fromIntegral r
+
+    coords = map row [0..imageHeight image - 1]
+    row i = map (\col -> (i, col)) [0..imageWidth image - 1]
