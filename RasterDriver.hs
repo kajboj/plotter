6b74061a14f4905629891959434b0d8f2fb940e2
Kajetan Bojko <kai@shutl.co.uk>
2014-10-26 21:46:48 +0000

    raster driver takes png filename from command line

--- RasterDriver.hs
+++ RasterDriver.hs
@@ -3,10 +3,12 @@ import Plotter.RasterDriver
 import Plotter.CommandWriter
 import Plotter.Shared
 import Plotter.PngParser
+import System.Environment
 
 main :: IO ()
 main = do
-  image <- parsePng "png/lenna64x64.png"
+  args <- getArgs
+  image <- parsePng (head args)
   commandWriter $ commands (drawPic image)
   where
     commands hpglCommands = hpglToCommands (x1, y1) (0, 0, 0, 0) hpglCommands
