530c700f96ddd246a41c3d18c43e2142d6a642f4
Kajetan Bojko <kai@shutl.co.uk>
2014-10-30 21:06:37 +0000

    removed unused curve and pixel drawing code from raster driver

--- Plotter/RasterDriver.hs
+++ Plotter/RasterDriver.hs
@@ -64,26 +64,6 @@ normalize (x, y) = (x/length, y/length)
   where
     length = sqrt $ x^2 + y^2
 
-drawGray1 :: (Float, Float) -> Float -> [HPGLCommand]
-drawGray1 (x, y) color = map MV coords
-  where
-    coords = map (\(x1, y1) -> (x+color*x1, y+color*y1)) [(1, 0), (1, 1), (0, 1), (0, 0)]
-
-drawGray2 :: (Float, Float) -> Float -> [HPGLCommand]
-drawGray2 (x, y) color = map MV coords
-  where
-    coords = [(x+color, y+color)]
-
-drawGray3 :: (Float, Float) -> Float -> [HPGLCommand]
-drawGray3 (x, y) color = map MV coords
-  where
-    coords = map f zigz
-    f (x1, y1) = (x+x1, y+y1)
-    zigz = map toF [1..zigCount] >>= zig
-    zig i = [((i-1)*zigDelta, color), (i*zigDelta, 0)]
-    zigDelta = 1 / (toF zigCount)
-    zigCount = maxZigs
-
 prefix :: Int -> Int -> [HPGLCommand]
 prefix width height = [SC (0, toF height, 0, toF width), PD]
 
@@ -92,48 +72,3 @@ suffix = [PU, MV (0, 0)]
 
 zipIndex :: [a] -> [(Int, a)]
 zipIndex = zip [0..]
-
-putCurve :: [String] -> IO ()
-putCurve curve = mapM_ putStrLn curve
-
-plot :: [Int] -> [String]
-plot curve = map (\n -> take n (repeat '*')) curve
-
-scale :: Int -> [Float] -> [Int]
-scale max curve = map (round . (* toF max)) curve
-
-sampleCurve :: (Float -> Float) -> Int -> [Float]
-sampleCurve f sampleCount = map (f . iToF) [0..(sampleCount-1)]
-  where
-   iToF i = (toF i) / (toF (sampleCount-1))
-
-fromAscii :: [String] -> Float -> Float
-fromAscii ascii x = case find (matchingRange x) (ranges $ normal ascii) of
-  Just ((minX, maxX), (minY, maxY)) -> minY + ((x - minX) / (maxX - minX) * (maxY - minY))
-  Nothing -> error $ "couldn't find value for " ++ show x
-
-  where
-    matchingRange x ((minX, maxX), _) = x >= minX && x <= maxX
-
-ranges :: [Float] -> [((Float, Float), (Float, Float))]
-ranges vals = zip (toRanges xs) (toRanges vals)
-  where
-    xs = map (norm . toF) [0..(length vals)]
-    norm = (/(toF $ length vals))
-    toRanges list = zip list $ (drop 1 list) ++ [last list]
-
-normal :: [String] -> [Float]
-normal ascii = map normalize lengths
-  where
-    lengths = map length ascii
-    normalize i = (toF i) / (toF $ maximum lengths)
-
-table = [ ""
-        , "***"
-        , "*****"
-        , "******"
-        , "*******"
-        , "*********"
-        , "*************"
-        , "*************************"
-        , "**********************************"]
