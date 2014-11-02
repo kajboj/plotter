417beca21a7f85814bc540d3388366e6d1f4fbe4
Kajetan Bojko <kai@shutl.co.uk>
2014-11-02 21:55:55 +0000

    path uses state monad

--- Plotter/Path.hs
+++ Plotter/Path.hs
@@ -3,27 +3,44 @@ module Plotter.Path where
 import qualified Data.Vector as V
 import qualified Data.List as L
 import qualified Data.Set as S
+import System.Random
+import Control.Monad.State
 
 type Grid a = V.Vector (V.Vector a)
 type Coords = (Int, Int)
 type Dimensions = (Int, Int)
 
-marks = L.concat $ repeat "abcdefghijklmnopqrstuvyz"
+marks = L.cycle "abcdefghijklmnopqrstuvyz"
 
 path :: Dimensions -> [Coords]
-path dims@(width, height) = reverse . snd $ tree dims [start] unvisited
+path dims@(width, height) = reverse $ evalState (tree dims [start]) unvisited
   where
     start = (0, 0)
     unvisited = S.delete start allNodes
     allNodes = S.fromList [(r, c) | r <- [0..height-1], c <- [0..width-1]]
 
-tree :: Dimensions -> [Coords] -> S.Set Coords -> (S.Set Coords, [Coords])
-tree dims path@(node:rest) unvisited = if S.null unvisited
-  then (unvisited, path)
-  else foldr visit (unvisited, path) neighbours
+tree :: Dimensions -> [Coords] -> State (S.Set Coords) [Coords]
+tree dims path@(node:rest) = do
+  unvisited <- get
+  put (S.delete node unvisited)
+  foldM visit path (neighbours unvisited)
   where
-    visit n (unv, path) = tree dims (n:path) (S.delete n unv)
-    neighbours = unvisitedNeighbours dims unvisited node 
+    visit path n = tree dims (n:path)
+    neighbours unvisited = unvisitedNeighbours dims unvisited node 
+
+shuffle :: [a] -> State StdGen [a]
+shuffle as = liftM (V.toList . foldr f v) $ rndIndexes $ length as
+  where
+    f i acc = let x = acc V.! i; y = acc V.! 0 in set i y (set 0 x acc)
+    v = V.fromList as
+
+rndIndexes :: Int -> State StdGen [Int]
+rndIndexes n = sequence $ replicate n randomInt
+  where
+    randomInt = do
+      rndGen <- get
+      let (i, rndGen') = randomR (0, n- 1) rndGen
+        in put rndGen' >> return i
 
 unvisitedNeighbours :: Dimensions -> S.Set Coords -> Coords -> [Coords]
 unvisitedNeighbours dims unvisited coords =
